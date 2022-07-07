7279a21195fd108a89de46203b5d275cc83ebb90
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gmail.com>
2022-07-07 19:51:37 +0700

    add searchbar components

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,5 +1,7 @@
 import { View, Text } from "react-native";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default function SearchBar() {
   return (
@@ -17,8 +19,34 @@ export default function SearchBar() {
             backgroundColor: "#eee",
             borderRadius: 50,
             flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
           },
         }}
+        renderLeftButton={() => (
+          <View style={{ marginLeft: 10 }}>
+            <Ionicons name="location-sharp" size={24} />
+          </View>
+        )}
+        renderRightButton={() => (
+          <View
+            style={{
+              flexDirection: "row",
+              marginRight: 8,
+              backgroundColor: "white",
+              padding: 9,
+              borderRadius: 30,
+              alignItems: "center",
+            }}
+          >
+            <AntDesign
+              name="clockcircle"
+              size={11}
+              style={{ marginRight: 6 }}
+            />
+            <Text>Search</Text>
+          </View>
+        )}
       />
     </View>
   );
